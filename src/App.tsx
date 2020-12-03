7b2295372833f8b2fb8066ef7aabed507e0d085c
Eirik Meland <eirik.meland@arktekk.no>
2020-12-03 21:14:43 +0100

    formatering

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <table>
-      <tr>
-        <td colSpan={4}>
-          Little Big Planet
-        </td>
-        </tr> 
-        <tr>
-          <td colSpan={4}>
-          Any% no overlord
-          </td>
-          </tr> 
-    
+        <table>
           <tr>
-            <td>
-              (bilde)     
-            </td>
-            <td>
-              tutorial
-            </td>
-            <td>
-              -1,2
-            </td>
-            <td>
-              0:48
-            </td>
+            <td colSpan={4}>Little Big Planet</td>
           </tr>
           <tr>
-            <td>
-              (bilde)     
-            </td>
-            <td>
-              konge
-            </td>
-            <td>
-              -7,5
-            </td>
-            <td>
-              5:55
-            </td>
+            <td colSpan={4}>Any% no overlord</td>
+          </tr>
+
+          <tr>
+            <td>(bilde)</td>
+            <td>tutorial</td>
+            <td>-1,2</td>
+            <td>0:48</td>
           </tr>
           <tr>
-            <td>
-              (bilde)     
-            </td>
-            <td>
-              afrika
-            </td>
-            <td>
-              (gjenstående tid til skjema tid)
-            </td>
-            <td>
-              29:13
-            </td>
+            <td>(bilde)</td>
+            <td>konge</td>
+            <td>-7,5</td>
+            <td>5:55</td>
           </tr>
           <tr>
-            <td>
-              (bilde)     
-            </td>
-            <td>
-              bruden
-            </td>
-            <td>
-              
-            </td>
-            <td>
-              33:10
-            </td>
+            <td>(bilde)</td>
+            <td>afrika</td>
+            <td>(gjenstående tid til skjema tid)</td>
+            <td>29:13</td>
+          </tr>
+          <tr>
+            <td>(bilde)</td>
+            <td>bruden</td>
+            <td></td>
+            <td>33:10</td>
           </tr>
           <tr>
             <td colSpan={4} align="right">
@@ -84,15 +48,13 @@ function App() {
               total time save: 0:06
             </td>
           </tr>
-          
+
           <tr>
             <td colSpan={4}>
-              <button>
-                start / stopp
-              </button>
+              <button>start / stopp</button>
             </td>
           </tr>
-         </table> 
+        </table>
       </header>
     </div>
   );
