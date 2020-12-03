db92afdb248b682ca0317a218cf4b93bce6bc106
Alio 849 <alfred@teig.ninja>
2020-12-03 21:01:42 +0100

    mock

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,93 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <table>
+      <tr>
+        <td colSpan={4}>
+          Little Big Planet
+        </td>
+        </tr> 
+        <tr>
+          <td colSpan={4}>
+          Any% no overlord
+          </td>
+          </tr> 
+    
+          <tr>
+            <td>
+              (bilde)     
+            </td>
+            <td>
+              tutorial
+            </td>
+            <td>
+              -1,2
+            </td>
+            <td>
+              0:48
+            </td>
+          </tr>
+          <tr>
+            <td>
+              (bilde)     
+            </td>
+            <td>
+              konge
+            </td>
+            <td>
+              -7,5
+            </td>
+            <td>
+              5:55
+            </td>
+          </tr>
+          <tr>
+            <td>
+              (bilde)     
+            </td>
+            <td>
+              afrika
+            </td>
+            <td>
+              (gjenstående tid til skjema tid)
+            </td>
+            <td>
+              29:13
+            </td>
+          </tr>
+          <tr>
+            <td>
+              (bilde)     
+            </td>
+            <td>
+              bruden
+            </td>
+            <td>
+              
+            </td>
+            <td>
+              33:10
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={4} align="right">
+              25:10
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={4} align="left">
+              total time save: 0:06
+            </td>
+          </tr>
+          
+          <tr>
+            <td colSpan={4}>
+              <button>
+                start / stopp
+              </button>
+            </td>
+          </tr>
+         </table> 
       </header>
     </div>
   );
