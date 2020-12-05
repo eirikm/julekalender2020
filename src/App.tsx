4974b826afe823317692e05c9ef2a2879e88571d
Alio 849 <alfred@teig.ninja>
2020-12-05 11:58:40 +0100

    bedre farger

--- src/App.tsx
+++ src/App.tsx
@@ -14,39 +14,66 @@ function App() {
             <td colSpan={4}>Any% no overlord</td>
           </tr>
 
-          <tr>
-            <td>(bilde)</td>
-            <td>tutorial</td>
-            <td style={{ color: "blue" }}>-1,2</td>
+          <tr style={{ backgroundColor: "#000" }}>
+            <td>
+              <img src="https://static.wikia.nocookie.net/littlebigplanet/images/a/aa/Popit_lbp_2.jpg"
+                width="60px"
+                height="60px"
+              />
+            </td>
+            <td align="left">tutorial</td>
+            <td style={{ color: "#0f0" }}>-1,2</td>
             <td align="right">0:48</td>
           </tr>
-          <tr>
-            <td>(bilde)</td>
-            <td>konge</td>
-            <td style={{ color: "pink" }}>-7,5</td>
-            <td>5:55</td>
+          <tr style={{ backgroundColor: "#222" }}>
+            <td>
+            <img src="https://cdn.staticneo.com/w/littlebigplanet/King.png"
+                width="60px"
+                height="60px"
+              />
+              </td>
+            <td align="left">konge</td>
+            <td style={{ color: "#f00" }}>+17,5</td>
+            <td align="right">5:55</td>
           </tr>
-          <tr style={{ backgroundColor: "grey" }}>
-            <td>(bilde)</td>
-            <td>afrika</td>
+          <tr style={{ backgroundColor: "#008" }}>
+            <td>
+            <img src="https://static.wikia.nocookie.net/littlebigplanet/images/d/d4/Zola.png"
+                width="60px"
+                height="60px"
+              />
+              </td>
+            <td align="left">afrika</td>
             <td>(gjenstående tid til skjema tid)</td>
-            <td>29:13</td>
+            <td align="right">29:13</td>
           </tr>
-          <tr>
-            <td>(bilde)</td>
-            <td>bruden</td>
+          <tr style={{ backgroundColor: "#222" }}>
+            <td>
+            <img src="https://static.wikia.nocookie.net/littlebigplanet/images/9/94/Frida.jpg"
+                width="60px"
+                height="60px"
+              />
+              </td>
+            <td align="left">bruden</td>
             <td></td>
-            <td>33:10</td>
+            <td align="right">33:10</td>
           </tr>
           <tr>
             <td colSpan={4} align="right">
-              <span style={{ color: "#f0f", fontSize: "500%" }}>25:10</span>
+              <span style={{ color: "#f00", fontSize: "500%" }}>25:10</span>
             </td>
           </tr>
           <tr>
             <td colSpan={4} align="left">
-              <span style={{ fontSize: "50%", color: 'green' }}>
+              <span style={{ fontSize: "80%", color: 'green' }}>
                 total time save: 0:06
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td colSpan={4} align="left">
+              <span style={{ fontSize: "80%", color: '#ff0000' }}>
+                total time loss: 0:06
               </span>
             </td>
           </tr>
