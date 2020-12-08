2b7d38cb558135f5477d3d05cd691be9498db557
Alio 849 <alfred@teig.ninja>
2020-12-08 20:38:38 +0100

    dagens

--- src/App.tsx
+++ src/App.tsx
@@ -14,44 +14,40 @@ function App() {
             <td colSpan={4}>Any% no overlord</td>
           </tr>
 
-          <tr style={{ backgroundColor: "#000" }}>
+          <tr className="oddetall">
             <td>
               <img src="https://static.wikia.nocookie.net/littlebigplanet/images/a/aa/Popit_lbp_2.jpg"
-                width="60px"
-                height="60px"
+                className="icon"
               />
             </td>
             <td align="left">tutorial</td>
-            <td style={{ color: "#0f0" }}>-1,2</td>
+            <td className="minustid">-1,2</td>
             <td align="right">0:48</td>
           </tr>
-          <tr style={{ backgroundColor: "#222" }}>
+          <tr className="partall">
             <td>
             <img src="https://cdn.staticneo.com/w/littlebigplanet/King.png"
-                width="60px"
-                height="60px"
+                className="icon"
               />
               </td>
             <td align="left">konge</td>
-            <td style={{ color: "#f00" }}>+17,5</td>
+            <td className="plusstid">+17,5</td>
             <td align="right">5:55</td>
           </tr>
-          <tr style={{ backgroundColor: "#008" }}>
+          <tr className="nå">
             <td>
             <img src="https://static.wikia.nocookie.net/littlebigplanet/images/d/d4/Zola.png"
-                width="60px"
-                height="60px"
+                className="icon"
               />
               </td>
             <td align="left">afrika</td>
             <td>(gjenstående tid til skjema tid)</td>
             <td align="right">29:13</td>
           </tr>
-          <tr style={{ backgroundColor: "#222" }}>
+          <tr className="partall">
             <td>
             <img src="https://static.wikia.nocookie.net/littlebigplanet/images/9/94/Frida.jpg"
-                width="60px"
-                height="60px"
+                className="icon"
               />
               </td>
             <td align="left">bruden</td>
@@ -60,19 +56,19 @@ function App() {
           </tr>
           <tr>
             <td colSpan={4} align="right">
-              <span style={{ color: "#f00", fontSize: "500%" }}>25:10</span>
+              <span className="klokke plusstid">25:10</span>
             </td>
           </tr>
           <tr>
             <td colSpan={4} align="left">
-              <span style={{ fontSize: "80%", color: 'green' }}>
+              <span className="litentekst minustid" >
                 total time save: 0:06
               </span>
             </td>
           </tr>
           <tr>
             <td colSpan={4} align="left">
-              <span style={{ fontSize: "80%", color: '#ff0000' }}>
+              <span className="litentekst plusstid ">
                 total time loss: 0:06
               </span>
             </td>
